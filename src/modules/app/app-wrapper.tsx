efe6c9e73e598c0b6b420f1a5435272f23e14897
IntriguedSayan <sayan.mid590@gmail.com>
2023-05-05 12:39:14 +0530

    Made the name & avatar in navbar dynamic by getting from cookie

--- src/modules/app/app-wrapper.tsx
+++ src/modules/app/app-wrapper.tsx
@@ -1,18 +1,45 @@
 import { useTheme } from "@emotion/react";
 import { AppBar, Avatar, Container, IconButton, Toolbar, Typography } from "@mui/material";
-
+import { getCookie } from "utils/cookies/cookies";
+import { useState, useEffect } from "react";
 interface props {
   children: React.ReactNode;
 }
 
 const AppWrapper: React.FC<props> = ({ children }) => {
-  const theme = useTheme();
 
-  function stringAvatar(name: string) {
+  const theme = useTheme();
+  const [userName,setUserName] = useState<string|boolean>(getCookie("user_name"));
+  
+  function stringAvatar(name: string):string {
     return `${name.split(" ")[0][0].toUpperCase()}${
       name.split(" ").length > 1 ? name.split(" ")[1][0].toUpperCase() : ""
     }`;
   }
+
+  const getName=(data:string | boolean):void=>{
+
+      
+
+      if(typeof(data)==="boolean"){
+
+        return;
+
+      }
+
+      let temp_userName = data;
+      let firstName=temp_userName.split(" ")[0];
+      const capitalizedFirstName = firstName.charAt(0).toUpperCase() + firstName.slice(1);
+
+      setUserName(capitalizedFirstName);
+
+  }
+
+  useEffect(()=>{
+
+    getName(userName);
+    
+  },[])
 
   return (
     <div>
@@ -24,9 +51,9 @@ const AppWrapper: React.FC<props> = ({ children }) => {
           <Toolbar disableGutters className="flex justify-between">
             <img src="https://masaischool.com/img/navbar/logo.svg" loading="lazy" />
             <div className="flex items-center gap-2">
-              <Typography color="black">Hello, sanjay</Typography>
+              <Typography color="black">{ userName }</Typography>
               <IconButton size="small" color="primary">
-                <Avatar className="bg-gray-500">{stringAvatar("sanjay")}</Avatar>
+                <Avatar className="bg-gray-500">{ typeof(userName)==="string" && stringAvatar(userName)}</Avatar>
               </IconButton>
             </div>
           </Toolbar>
